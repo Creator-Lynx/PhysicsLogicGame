d53a153c433d5899461db37077befe913ad3d5f5
Creator-Lynx <1_linx_1@mail.ru>
2022-03-16 20:39:17 +0300

    fix_cubes_ui_and_start_12_2lvl

--- Assets/Scripts/CubeArrowsSpawner.cs
+++ Assets/Scripts/CubeArrowsSpawner.cs
@@ -14,6 +14,11 @@ public class CubeArrowsSpawner : MonoBehaviour
         {
             GameObject arr = Instantiate(ArrowsPrefab, transform.position + new Vector3(0f, 0.51f, 0f),
             Quaternion.Euler(0f, 0f, 0f), transform);
+            arr.transform.localScale = new Vector3(1 / transform.localScale.x, 1, 1 / transform.localScale.z);
+            RectTransform[] uis = arr.GetComponentsInChildren<RectTransform>();
+            uis[0].localPosition = new Vector3(-(transform.localScale.x - 1) / 2, 0, 0);
+            uis[2].localPosition = new Vector3((transform.localScale.x - 1) / 2, 0, 0);
+            //uis[1].localPosition = new Vector3(transform.localScale.x / 2, transform.localScale.y / 2, 0);
             arr.GetComponent<Animation>().Play();
             Destroy(arr, 4f);
         }
@@ -21,6 +26,10 @@ public class CubeArrowsSpawner : MonoBehaviour
         {
             GameObject arr = Instantiate(ArrowsPrefab, transform.position + new Vector3(0f, 0.51f, 0f),
             Quaternion.Euler(0f, 90f, 0f), transform);
+            arr.transform.localScale = new Vector3(1 / transform.localScale.z, 1, 1 / transform.localScale.x);
+            RectTransform[] arrowRects = arr.GetComponentsInChildren<RectTransform>();
+            arrowRects[0].localPosition = new Vector3(-(transform.localScale.z - 1) / 2, 0, 0);
+            arrowRects[2].localPosition = new Vector3((transform.localScale.z - 1) / 2, 0, 0);
             arr.GetComponent<Animation>().Play();
             Destroy(arr, 4f);
         }
