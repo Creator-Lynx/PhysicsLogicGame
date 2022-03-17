565d696fff75eed1c3d3fee628c513da58162eed
Creator-Lynx <1_linx_1@mail.ru>
2022-03-17 16:11:33 +0300

    add_lvl_12_2_with_onelvl_mechanic_script

---
+++ Assets/Scenes/lvl 12_2/rigidVelocityChecker.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rigidVelocityChecker : MonoBehaviour
+{
+    Rigidbody rig;
+    [SerializeField]
+    float zeroMagnitude = 0.08f;
+    [SerializeField]
+    float timeToChange = 1f;
+    void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
+
+    int counterToChange = 0;
+    bool counting = true;
+    void FixedUpdate()
+    {
+        if (!counting) return;
+        if (rig.velocity.magnitude > zeroMagnitude)
+        {
+            counterToChange++;
+            if (counterToChange > timeToChange / Time.fixedDeltaTime)
+                ChangeScale();
+        }
+    }
+
+    void ChangeScale()
+    {
+        Debug.Log("Change scale " + gameObject.name);
+        counting = false;
+        if (GetComponent<Player>().isVert)
+        {
+            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y,
+            ScaleCalc(transform.localScale.z));
+        }
+        if (GetComponent<Player>().isHor)
+        {
+            transform.localScale = new Vector3(ScaleCalc(transform.localScale.x), transform.localScale.y,
+            transform.localScale.z);
+        }
+    }
+    int ScaleCalc(float x)
+    {
+        x = x / 2 + 0.5f;
+        if (x > 1) return (int)x;
+        else return 1;
+    }
+}
