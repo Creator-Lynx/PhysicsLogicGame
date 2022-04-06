9e3b33f51e4cd5c4a8c775a6bf02caca8c40b088
Creator-Lynx <1_linx_1@mail.ru>
2022-04-06 11:51:37 +0300

    add level 20_2
    change fade cube script to its able to work on his childs
    add level 20_2

--- Assets/Scripts/CubeFadeMechanic/FadeCubeAction.cs
+++ Assets/Scripts/CubeFadeMechanic/FadeCubeAction.cs
@@ -7,21 +7,31 @@ public class FadeCubeAction : MonoBehaviour, IButtonActivatable
     [SerializeField]
     Material main, transparent;
     [SerializeField] bool state = true;
+
+    Collider[] childsColl;
+    MeshRenderer[] childsMesh;
     void Start()
     {
-        GetComponent<MeshRenderer>().material = state ? main : transparent;
-        GetComponent<Collider>().isTrigger = !state;
+        //GetComponent<MeshRenderer>().material = state ? main : transparent;
+        //GetComponent<Collider>().isTrigger = !state;
+        childsMesh = GetComponentsInChildren<MeshRenderer>();
+        childsColl = GetComponentsInChildren<Collider>();
+        for (int i = 0; i < childsColl.Length; i++)
+        {
+            childsColl[i].isTrigger = !state;
+            childsMesh[i].material = state ? main : transparent;
+        }
     }
     public void ButtonAction()
     {
         state = !state;
-        GetComponent<MeshRenderer>().material = state ? main : transparent;
-        GetComponent<Collider>().isTrigger = !state;
+        //GetComponent<MeshRenderer>().material = state ? main : transparent;
+        //GetComponent<Collider>().isTrigger = !state;
+        for (int i = 0; i < childsColl.Length; i++)
+        {
+            childsColl[i].isTrigger = !state;
+            childsMesh[i].material = state ? main : transparent;
+        }
     }
-    public enum CubeFadeActionMode
-    {
-        switchMat,
-        defaultMat,
-        transparentMat
-    }
+
 }
